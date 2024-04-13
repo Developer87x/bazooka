661c1e7c501f9f245af3c5a83ea40d1959ec65c1
developer87x <mohammed_949@hotmail.com>
2024-04-13 22:28:05 +0300

    commit

--- src/Services/Customers/Bazooka.Customers.Api/Program.cs
+++ src/Services/Customers/Bazooka.Customers.Api/Program.cs
@@ -1,11 +1,30 @@
+using Bazooka.Customers.Api.Applications.Validations;
+using Bazooka.Customers.Api.Infrastructure;
+using Bazooka.Customers.Api.Infrastructure.Repositories;
+using Bazooka.Customers.Api.Infrastructure.Services;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var logger= new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).Enrich.FromLogContext().CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
+
+logger.Debug("Application started..");
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// dbcontext 
+builder.Services.AddDbContext<CustomersContext>(cfg=>cfg.UseSqlServer(builder.Configuration["ConnectionString"]),ServiceLifetime.Scoped);
+builder.Services.AddValidatorsFromAssemblyContaining<CustomerValidation>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
@@ -17,7 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
